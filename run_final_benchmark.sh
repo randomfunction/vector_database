11984f0ee5e9c4d6147eedf34a141dc3cdbc9e3e
randomfunction <tanishqparihar3@gmail.com>
2026-06-24 13:50:54 +0530

    restructured folder

--- run_final_benchmark.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "==========================================="
-echo "COMPILING SCALAR BUILD"
-echo "g++ -std=c++17 -O3 -march=native -ffast-math -Isrc final_benchmark.cpp src/database.cpp -o bench_final"
-g++ -std=c++17 -O3 -march=native -ffast-math -Isrc final_benchmark.cpp src/database.cpp -o bench_final
-
-echo ""
-echo "RUNNING BENCHMARK..."
-./bench_final
